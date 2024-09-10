9709e7bfc79afe18b2f0bf5f710c89885d90274f
Nikhil059 <99722876+Nikhil059@users.noreply.github.com>
2024-09-10 05:25:22 +0000

    01_variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let accountName = "SGIT";
+var accountcity = "Jaipur";
+accountholder = "Shivam";
+
+//const can't be change
+
+accountName = "SSS"
+accountcity = "Bengaluru";
+accountholder = "Ayush"
+
+// prefer not to use var as its scope is global and can impact others data
+
+console.log([accountName,accountcity,accountholder]);
+
