11865998aad9ade3f4de63f36ff6c9115f504c4b
Nikhil059 <99722876+Nikhil059@users.noreply.github.com>
2024-09-11 10:35:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//datatypes
+//string, null(standalone value),undefined,boolean ,number,bigint,symbol 
+
+console.log(typeof null); // null is a type of Object
+console.log(typeof undefined); // undefined is a type of undefined itself
+
+let isLoggedIn = true;
+console.log(typeof isLoggedIn);
