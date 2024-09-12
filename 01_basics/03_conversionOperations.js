7302bb2bc63366f751c4e2880c876aa4f7a36b00
Nikhil059 <99722876+Nikhil059@users.noreply.github.com>
2024-09-12 05:24:55 +0000

    conversionOerations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = 33;
+
+console.log(score);
+// console.log(typeof score)
+let converIntoString = String(score);
+console.log(typeof converIntoString);
+
+
+let num = "12abs"
+
+console.log(typeof num);
+let convertIntoNumber = Number(num)
+console.log(convertIntoNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false = > 0
+
+let isLoggegIn = "asdfg hejd";
+let convertIntoBoolean = Boolean(isLoggegIn);
+console.log(convertIntoBoolean);
+
+// 1 => true
+// 0=> false
+// "" => false
+// "anything" => true
+
+
+
+
